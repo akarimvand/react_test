a993a08e7a7fab21d2ededc2c5e72fad4dce757c
Amin naseri karimvand <akarimvand@gmail.com>
2025-12-10 22:06:35 +0330

    Add files via upload

---
+++ components/icons/LogoIcon.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const LogoIcon: React.FC = () => (
+    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 2L2 7V17L12 22L22 17V7L12 2Z" fill="#6366F1"/>
+        <path d="M12 12L22 7L12 2L2 7L12 12Z" fill="#A5B4FC"/>
+        <path d="M2 17L12 12V22L2 17Z" fill="#818CF8"/>
+        <path d="M22 17L12 12V22L22 17Z" fill="#4F46E5"/>
+    </svg>
+);
+
+export default LogoIcon;
